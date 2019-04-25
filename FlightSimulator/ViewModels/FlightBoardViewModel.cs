d5be7db2caa6fbdab48e679e14e7594e142ff275
yuvalmor94 <yuvalmor94@gmail.com>
2019-04-25 18:15:38 +0300

    importing proect

---
+++ FlightSimulator/ViewModels/FlightBoardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using FlightSimulator.Model.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.ViewModels
+{
+    public class FlightBoardViewModel : BaseNotify
+    {
+        public double Lon{get; }
+        public double Lat { get; }
+    }
+}
