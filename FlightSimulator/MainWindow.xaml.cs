2560ddb9236f545133f4313af20f4aea8f85e6fc
yuvalmor94 <yuvalmor94@gmail.com>
2019-04-26 19:21:44 +0300

    update main window, with the correct joystick controller

--- FlightSimulator/MainWindow.xaml.cs
+++ FlightSimulator/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace FlightSimulator
         {
             InitializeComponent();
         }
+
+        private void FlightBoard_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
