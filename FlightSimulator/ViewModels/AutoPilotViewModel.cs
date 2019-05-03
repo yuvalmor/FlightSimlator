f3f058c695bf53ad4dbd2a13ea82f1e1680c731f
yuvalmor94 <yuvalmor94@gmail.com>
2019-05-03 17:58:36 +0300

    clear command working - auto pilot

--- FlightSimulator/ViewModels/AutoPilotViewModel.cs
+++ FlightSimulator/ViewModels/AutoPilotViewModel.cs
@@ -1,14 +1,39 @@
-﻿using System;
+﻿using FlightSimulator.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace FlightSimulator.ViewModels
 {
-    class AutoPilotViewModel
+    class AutoPilotViewModel : BaseNotify
     {
-        string myText;
-        public string MyText { get => myText; set => myText = value; }
+        private string textCommand;
+        public string TextCommand
+        {
+            get=> textCommand; 
+            set
+            {
+                if (textCommand != value)
+                {
+                    textCommand = value;
+                    NotifyPropertyChanged("textCommand");
+                }
+            }
+        }
+        private ICommand _clearCommand;
+        public ICommand ClearCommand {
+            get
+            {
+                return _clearCommand ?? (_clearCommand = new CommandHandler(() => ClearText()));
+            }
+        }
+
+        private void ClearText()
+        {
+            TextCommand = "";
+        }
     }
 }
